177b98d82c9e90729b4e08a2724eb61a53a44be3
pablonolasco <pablo.nolasco@live.com.mx>
2021-02-09 23:03:56 -0600

    variables constante

---
+++ 02-Variables/js/03-app.js
@@ -0,0 +1,11 @@
+// const no puede ser reasignado
+const producto = "Monitor";
+
+producto = "Teclado";
+
+console.log(producto);
+
+// Siempre inicializar la variable
+const precio;
+
+console.log(precio);
