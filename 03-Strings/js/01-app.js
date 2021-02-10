549b9965e62db211759148ed9b91c4ca5aabc565
pablonolasco <pablo.nolasco@live.com.mx>
2021-02-09 23:20:28 -0600

    creando strings

--- 03-Strings/js/01-app.js
+++ 03-Strings/js/01-app.js
@@ -1,6 +1,6 @@
 // Escapar cadena de texto
 const producto = "Monitor 24\"";
-const producto2= String('Monitor 20"');
+const producto2= String('Monitor 20 "');
 const producto3= new String("Monitor 27 pulgadas");
 
 console.log(producto);
