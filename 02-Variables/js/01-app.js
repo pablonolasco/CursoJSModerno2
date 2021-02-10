e1168cacb51a94390a622f8984637da7f5b3a79d
pablonolasco <pablo.nolasco@live.com.mx>
2021-02-09 22:47:47 -0600

    utilizando var

---
+++ 02-Variables/js/01-app.js
@@ -0,0 +1,22 @@
+// Inicializar una variable
+var producto = 20;
+console.log(producto);
+
+// Las variables se pueden reasignar
+producto = "Monitor de 19 pulgadas";
+console.log(producto);
+
+// JavaScript es un lenguaje de tipo dinamico, no se especifica tipo de dato
+producto = 10.5;
+console.log(producto);
+// Se pueden inicializar sin valor y asignarlo despues.
+var disponible;
+disponible = true;
+// Inicializar multiples variables
+var categoria = "Computadora",
+    marca = "LG",
+    calificacion = 10;
+
+var nombreProducto;
+var nombre_producto;
+var NombreClase;
