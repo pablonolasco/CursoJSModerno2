8a7a865094018f4951e9cc1ccce56a0519e6293d
pablonolasco <pablo.nolasco@live.com.mx>
2021-02-09 23:19:56 -0600

    creando string

---
+++ 03-Strings/js/01-app.js
@@ -0,0 +1,14 @@
+// Escapar cadena de texto
+const producto = "Monitor 24\"";
+const producto2= String('Monitor 20"');
+const producto3= new String("Monitor 27 pulgadas");
+
+console.log(producto);
+console.log(producto2);
+console.log(producto3);
+
+
+
+
+
+
