a578228bf37287e361317c63d8f85d23ad843f15
pablonolasco <pablo.nolasco@live.com.mx>
2021-02-09 23:32:51 -0600

    methodos

---
+++ 03-Strings/js/02-app.js
@@ -0,0 +1,11 @@
+// String Methods
+const producto = "Monitor 20 Pulgadas";
+
+// Tamaño de una cadena
+console.log(producto.length);
+
+// Existe en la cadena ala palabra Existe = 0, No existe = -1
+console.log(producto.indexOf("Monitor"));
+
+// Existe en la cadena la palabra Existe = true, No existe = false;
+console.log(producto.includes("monitor"));
