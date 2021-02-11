fbaea6f8335127b2f2a3eb5ad095c4fd3ccb4b4d
pablonolasco <pablo.nolasco@live.com.mx>
2021-02-11 17:12:35 -0600

    concatenacion de strings

---
+++ 03-Strings/js/03-app.js
@@ -0,0 +1,7 @@
+const producto = "Monitor de 24 pulgadas ";
+const precio = "30 USD";
+
+console.log(producto.concat(precio));
+
+console.log('El producto es ' + producto + ' tiene un precio de ' + precio);
+console.log(`El producto ${producto} tiene un precio ${precio}`);
