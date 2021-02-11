69abf707d62cc5a7aeb2ec54b0ecc5bc000b5378
pablonolasco <pablo.nolasco@live.com.mx>
2021-02-11 17:56:49 -0600

    repeat y split

---
+++ 03-Strings/js/06-app.js
@@ -0,0 +1,9 @@
+const producto = 'Monitor 20 pulgadas';
+
+//.repeat repite el elemento, redondea el numero
+console.log(producto.repeat(2.4));
+
+// .split separalos elementos dependiendo del caracter
+const hobbies = "Leer,Dormir,Musica,Videojuegos";
+
+console.log(hobbies.split(','));
