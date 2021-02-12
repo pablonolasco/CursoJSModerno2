6463dd86a7457061d1d2605d2b68d540bc2658c5
pablonolasco <pablo.nolasco@live.com.mx>
2021-02-11 19:26:31 -0600

    conversiones

---
+++ 04-Numeros/js/06-app.js
@@ -0,0 +1,10 @@
+const numero1 = "20",
+    numero2 = "20.2",
+    numero3 = "Uno",
+    numero4 = 20;
+
+// conversion
+console.log(Number.parseInt(numero1));
+console.log(Number.parseFloat(numero2));
+// revision si es entero
+console.log(Number.isInteger(numero3));
