b34005eb9a8054d0670bd5e3d88ded628549b3ef
pablonolasco <pablo.nolasco@live.com.mx>
2021-02-11 18:07:27 -0600

    toLowerCase, toUpperCase y tostring

---
+++ 03-Strings/js/07-app.js
@@ -0,0 +1,12 @@
+const producto = 'Monitor 20 pulgadas';
+
+// .toLowerCase() minuscula
+console.log(producto.toLowerCase());
+
+// .toUpperCase() mayuscula
+console.log(producto.toUpperCase());
+
+// conversion a cadena
+const precio = 300;
+console.log(precio.toString());
+console.log(precio);
