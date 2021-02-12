27ac9dd51461b7cf925dc572a835b8d3348b3fea
pablonolasco <pablo.nolasco@live.com.mx>
2021-02-11 18:57:49 -0600

    metodos math

---
+++ 04-Numeros/js/03-app.js
@@ -0,0 +1,34 @@
+let resultado;
+
+// PI
+
+resultado = Math.PI;
+
+// round
+resultado = Math.round(2.4);
+resultado = Math.round(2.9);
+
+// redondear hacia arriba
+resultado = Math.ceil(2.1);
+
+// redondear hacia abajo
+resultado = Math.floor(2.9);
+
+// raiz cuadrada
+resultado = Math.sqrt(9);
+
+// valor absoluto, le quita el negativo
+resultado = Math.abs(-500);
+
+// potencia
+resultado = Math.pow(2, 4);
+
+// minimo
+resultado = Math.min(3, 4, 9);
+
+// maximo
+resultado = Math.max(3, 4, 9);
+
+// aleatorio numeros del 1 al 30
+resultado = Math.floor(Math.random() * 30);
+console.log(resultado);
