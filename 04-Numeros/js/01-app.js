885ef41fa3c49cf92231667f1cebcfc99aa88d67
pablonolasco <pablo.nolasco@live.com.mx>
2021-02-11 18:19:08 -0600

    declaracion

---
+++ 04-Numeros/js/01-app.js
@@ -0,0 +1,12 @@
+const numero1 = 20,
+    numero2 = 20.50,
+    numero3 = -20,
+    numero4 = .1280;
+console.log(numero1);
+console.log(numero2);
+console.log(numero3);
+console.log(numero4);
+
+// otra forma de declaracion
+const num = new Number(4);
+console.log(num);
